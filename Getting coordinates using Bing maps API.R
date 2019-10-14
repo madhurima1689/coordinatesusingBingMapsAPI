0a1bfac616f6eb851deac21d2baff5683611f1ec
madhurima1689 <50331731+madhurima1689@users.noreply.github.com>
2019-10-14 16:20:51 +0530

    Add files via upload

---
+++ Getting coordinates using Bing maps API.R
@@ -0,0 +1,31 @@
+#Setting up working directory
+setwd("C:/Users/mchoudhu/Documents/PGP-BABI")
+
+#BingMaps API = AmQ2wYrNc-ifHwCws5q0J0nynWSokmRzu-49B53Esp-Hq7x5STo_XbLVw0GOgPc0
+
+places=read.csv("Latlong.csv",header = TRUE)
+attach(places)
+places$concat=paste(place,",",state)
+
+
+install.packages("data.table")
+library(data.table)
+devtools::install_github("gsk3/taRifx.geo")
+library(taRifx.geo)
+options(BingMapsKey='AmQ2wYrNc-ifHwCws5q0J0nynWSokmRzu-49B53Esp-Hq7x5STo_XbLVw0GOgPc0')
+
+final=data.frame()
+temp.df=data.frame()
+for (i in 1:nrow(places)) 
+  {
+  var=places[i,3]
+  temp = geocode(var, service="bing", returntype="coordinates")
+  temp.df=as.data.frame(t(temp))
+  temp.df$place=places[i,3]
+  final = as.data.table(rbind(final,temp.df))
+  
+}
+
+colnames(final) = c("Latitude","Longitude", "Concatenated place name")
+
+write.csv(final,"Coordinates_Bingmaps.csv")
