786705cf9fed73678074e28c2d483d3cf343054e
madhurima1689 <50331731+madhurima1689@users.noreply.github.com>
2019-10-15 12:19:39 +0530

    Update Getting coordinates using Bing maps API.R

--- Getting coordinates using Bing maps API.R
+++ Getting coordinates using Bing maps API.R
@@ -1,4 +1,4 @@
-#Setting up working directory
+#Setting up working directory, from where the required input file with place names will be imported
 setwd("C:/Users/mchoudhu/Documents/PGP-BABI")
 
 #BingMaps API = AmQ2wYrNc-ifHwCws5q0J0nynWSokmRzu-49B53Esp-Hq7x5STo_XbLVw0GOgPc0
@@ -13,7 +13,7 @@ library(data.table)
 devtools::install_github("gsk3/taRifx.geo")
 library(taRifx.geo)
 options(BingMapsKey='AmQ2wYrNc-ifHwCws5q0J0nynWSokmRzu-49B53Esp-Hq7x5STo_XbLVw0GOgPc0')
-#You can generate your own Bing Mapis API key from here - https://docs.microsoft.com/en-us/bingmaps/getting-started/bing-maps-dev-center-help/getting-a-bing-maps-key
+#You can generate your own Bing Maps API key from here - https://docs.microsoft.com/en-us/bingmaps/getting-started/bing-maps-dev-center-help/getting-a-bing-maps-key
 final=data.frame()
 temp.df=data.frame()
 for (i in 1:nrow(places)) 
